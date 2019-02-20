7421afb9df2de5890780888e10f75a0fbe1575e6
Prasant Solanki <prashants975@gmail.com>
2019-02-20 15:56:16 +0530

    yaxis

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+// scrolling effect on anchor
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+  
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        var hash = this.hash;
+  
+         $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          window.location.hash = hash;
+        });
+      } 
+    });
+  });
